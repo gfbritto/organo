ab5834d8d0f8c1740114ab7ac7efd44ca84f2716
gfbritto <gfbritto@outlook.com>
2022-09-28 08:46:43 -0300

    feat: add components do index page

---
+++ src/components/InputText/index.js
@@ -0,0 +1,11 @@
+import './InputText.css'
+const InputText = () => {
+    return (
+        <div className='input-text'>
+            <label>Nome</label>
+            <input placeholder='Insira o seu nome'></input>
+        </div>
+    )
+}
+
+export default InputText
